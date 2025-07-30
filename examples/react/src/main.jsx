917ee999d301b2d7082a0c7b941028bba885c299
Christopher Timmerman <christopher.timmerman@boomi.com>
2025-07-30 10:07:58 -0500

    updates for stackblitz

--- examples/react/src/main.jsx
+++ examples/react/src/main.jsx
@@ -7,7 +7,7 @@ import UsersPage from './pages/users';
 import IntegrationsPage from './pages/integrations';
 import DashboardPage from './pages/dashboard';
 import Users from './pages/users';
-import Root from './Root';
+import Root from './root';
 import ProtectedRoute from './protectedRoutes';
 import { AuthContextProvider } from './context/authContext';
 
